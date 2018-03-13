484578f218e7951fff7d1c5b925b1f9a795ec3df
Fabian Engels <fe@chiari.de>
2018-03-13 13:38:48 +0100

    [Task] Working fetch from remote

--- src/main/java/org/chiari/jira/mitelink/customfields/MiteProjectSelector.java
+++ src/main/java/org/chiari/jira/mitelink/customfields/MiteProjectSelector.java
@@ -15,6 +15,14 @@ import com.atlassian.jira.issue.search.SearchContextImpl;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,48 +30,78 @@ import java.util.Map;
 @Scanned
 public class MiteProjectSelector extends SelectCFType {
 
-    @ComponentImport JiraBaseUrls jiraBaseUrls;
-    @ComponentImport OptionsManager optionsManager;
-    @ComponentImport GenericConfigManager genericConfigManager;
-    @ComponentImport CustomFieldValuePersister customFieldValuePersister;
+    // TODO@FE: move this to plugin config
+    private String APIEndpoint = "https://kompass-chiari.mite.yo.lk/projects.json?api_key=25cfb27ce28cc08d";
+
+    @ComponentImport
+    JiraBaseUrls jiraBaseUrls;
+    @ComponentImport
+    OptionsManager optionsManager;
+    @ComponentImport
+    GenericConfigManager genericConfigManager;
+    @ComponentImport
+    CustomFieldValuePersister customFieldValuePersister;
 
     public MiteProjectSelector(CustomFieldValuePersister customFieldValuePersister, OptionsManager optionsManager, GenericConfigManager genericConfigManager, JiraBaseUrls jiraBaseUrls) {
         super(customFieldValuePersister, optionsManager, genericConfigManager, jiraBaseUrls);
         this.optionsManager = optionsManager;
         this.genericConfigManager = genericConfigManager;
         this.customFieldValuePersister = customFieldValuePersister;
-
         this.jiraBaseUrls = jiraBaseUrls;
-
     }
 
     @Override
     public Map<String, Object> getVelocityParameters(Issue issue, CustomField field, FieldLayoutItem fieldLayoutItem) {
+
+        System.out.println("--- [DEBUG] Calling getVelocityParameters() ---");
+
         Map params = super.getVelocityParameters(issue, field, fieldLayoutItem);
 
         FieldConfig fieldConfig = null;
+
         if (issue == null) {
+            System.out.println("--- [DEBUG] Issue is null ---");
             fieldConfig = field.getReleventConfig(new SearchContextImpl());
         } else {
+            System.out.println("--- [DEBUG] Issue is not null ---");
+            System.out.println(String.format("--- [DEBUG] Issue ID: %d ---", issue.getId()));
             fieldConfig = field.getRelevantConfig(issue);
         }
 
         Options options = this.optionsManager.getOptions(fieldConfig);
         if (options.isEmpty()) {
-            this.optionsManager.createOption(fieldConfig, null, new Long(1), "Option 1");
-            this.optionsManager.createOption(fieldConfig, null, new Long(2), "Option 2");
-            this.optionsManager.createOption(fieldConfig, null, new Long(3), "Foo");
+
+            System.out.println("--- [DEBUG] Fetching options ---");
+
+            ArrayList<JSONObject> projectData = GetProjectList();
+
+            //for (int i = 0; i < projectData.size(); i++) {
+            for (int i = 0; i < 10; i++) {
+
+                String identifier = "";
+
+                if (projectData.get(i).has("customer_name")) {
+                    identifier = projectData.get(i).getString("customer_name") + " - ";
+                }
+
+                identifier += projectData.get(i).getString("name");
+
+                this.optionsManager.createOption(fieldConfig, null, null, identifier);
+            }
+
+        } else {
+            System.out.println("--- [DEBUG] Options already present ---");
         }
         options = this.optionsManager.getOptions(fieldConfig);
 
-        Map < Long, String > results = new HashMap<>();
-        Long selectedId = (long) - 1;
+        Map<Long, String> results = new HashMap<>();
+        Long selectedId = (long) -1;
         boolean selected = false;
         Object value = field.getValue(issue);
         if (value != null) {
-        selected = true;
+            selected = true;
         }
-        for (Option option: options) {
+        for (Option option : options) {
             results.put(option.getOptionId(), option.getValue());
 
             if (selected && value.toString().equals(option.getValue())) {
@@ -71,9 +109,50 @@ public class MiteProjectSelector extends SelectCFType {
             }
         }
 
-        params.put("results", results);
+        params.put("options", results);
         params.put("selectedId", selectedId);
 
         return params;
+    }
+
+    private String SendGETRequest(String url) {
+
+        StringBuffer response = new StringBuffer();
+
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response.toString();
+    }
+
+    private ArrayList<JSONObject> GetProjectList() {
+
+        ArrayList<JSONObject> projects = new ArrayList<>();
+
+        String response = SendGETRequest(this.APIEndpoint);
+
+        JSONArray jsonarray = new JSONArray(response);
+        for (int i = 0; i < jsonarray.length(); i++) {
+            JSONObject jsonobject = jsonarray.getJSONObject(i);
+            JSONObject project = jsonobject.getJSONObject("project");
+            projects.add(project);
+        }
+
+        return projects;
     }
 }
